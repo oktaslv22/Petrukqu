e3e3e63284c3fc009edaae9d94a839b8ddd314a5
Okta.slv <62434433+oktaslv22@users.noreply.github.com>
2020-03-26 16:10:22 +0700

    Update Matriks3x3.cpp

--- Matriks3x3.cpp
+++ Matriks3x3.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int i,j,matriks[3][3],*det;
+    int i,j,matriks[3][3],det;
+	int *determinan;
+	determinan = &det;
 	  cout<<"\t\t\tMenghitung determinan matriks 3×3 \n\n\n";
 	
 	for (i=1; i<=3; i++){
 		for (j=1; j<=3; j++){
 			cout << "input matriks ke "<<i<<", "<<j<<" = ";
-  dah lah gamau sok tau :(( nti salah:((
-	  yok bisa yok *oktas,nadia
+			cin >> matriks[i-1][j-1];
+		}
+	}
+	// yok lanjut @din @nad
   
-  kan blom dibenerin ke pointer punya w din @dina wkwkwwk
-   ohh iya astaga kek mana ahaha
-	  @nadia nad gmn:(
 return 0;
 }
